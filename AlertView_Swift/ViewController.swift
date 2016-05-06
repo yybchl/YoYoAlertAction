454f6f74bb77160034feb91b005b9fb9c860683e
杨友彬 <987311160@qq.com>
2016-05-06 10:16:27 +0800

    Initial Commit

---
+++ AlertView_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertView_Swift
+//
+//  Created by YoYo on 16/5/6.
+//  Copyright © 2016年 cn.yoyoy.mw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
